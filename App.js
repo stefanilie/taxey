a63938d3bb7e57a7c36c8d1d7f9bb5d5487fda25
Stefan Ilie <ilie.stefan92@yahoo.com>
2021-02-10 16:20:17 +0000

    [compute] add THP

--- App.js
+++ App.js
@@ -12,18 +12,29 @@ import { StatusBar } from 'react-native';
 import { View } from 'react-native';
 import { Button, ButtonGroup, Input } from 'react-native-elements';
 import {
+  ALLOWANCE,
   ANNUAL_TO_THP,
   ANNUAL_TO_THP_COPY,
+  BASIC_RATE,
+  HIGHER_RATE,
   MARGIN,
   THP_TO_ANNUAL_COPY,
+  NI_1920_MTH_ALLOWANCE,
+  NI_2021_MTH_ALLOWANCE,
+  NI_1920_MTH_UEL,
+  NI_BASIC_RATE,
+  ADDITIONAL_RATE,
+  NI_UEL_RATE,
 } from './constants';
 import { hp, wp } from './utils';
 
 const App: () => React$Node = () => {
-  const [calculatorMode, setCalculatorMode] = useState(0);
-  const [displayResults, setDisplayResults] = useState(false);
   const [value, setValue] = useState(0);
+  const [calculatorMode, setCalculatorMode] = useState(0);
+  const [taxYear, setTaxYear] = useState(0);
+  const [displayResults, setDisplayResults] = useState(false);
   const [computedValue, setComputedValue] = useState(0);
+  const [details, setDetails] = useState({});
 
   const placeholderDecider = () => {
     return calculatorMode === ANNUAL_TO_THP
@@ -31,11 +42,66 @@ const App: () => React$Node = () => {
       : THP_TO_ANNUAL_COPY;
   };
 
-  const handleClickCompute = () => {
-    // do the calculus.
-    setComputedValue(value);
-    setDisplayResults(!displayResults);
+  const computeUELIncomeTax = () => {
+    const availableAllowance = Math.abs(ALLOWANCE - (value - 100000) / 2);
+    const withoutAllowance =
+      value > 100000 ? 50000 - availableAllowance : 50000 - ALLOWANCE;
+    return withoutAllowance * BASIC_RATE + (value - 50000) * HIGHER_RATE;
   };
+
+  const computeIncomeTax = () => {
+    switch (true) {
+      case value <= ALLOWANCE:
+        return 0;
+      case value > ALLOWANCE && value <= 50000: {
+        return BASIC_RATE * (value - ALLOWANCE);
+      }
+      case value > 50000 && value <= 150000:
+        return computeUELIncomeTax();
+      case value > 150000: {
+        const upTo15k = computeIncomeTax(150000);
+        return upTo15k + (value - 150000) * ADDITIONAL_RATE;
+      }
+    }
+  };
+
+  const computeNationalInsurance = (monthlyAllowance) => {
+    const allowance = monthlyAllowance * 12;
+    const uelLimit = NI_1920_MTH_UEL * 12;
+    const upToUEL = (value - allowance) * NI_BASIC_RATE;
+    switch (true) {
+      case value <= allowance:
+        return 0;
+      case value > allowance && value <= uelLimit:
+        return upToUEL;
+      case value > uelLimit:
+        return upToUEL + (value - uelLimit) * NI_UEL_RATE;
+    }
+  };
+
+  const handleClickCompute = () => {
+    const incomeTax = computeIncomeTax();
+    const ni20 = computeNationalInsurance(NI_1920_MTH_ALLOWANCE);
+    const ni21 = computeNationalInsurance(NI_2021_MTH_ALLOWANCE);
+    const valueFor20 = value - incomeTax - ni20;
+    const valueFor21 = value - incomeTax - ni21;
+    setDetails({
+      valueFor20: valueFor20,
+      valueFor21: valueFor21,
+      ni20: ni20,
+      ni21: ni21,
+    });
+    taxYear === 0 ? setComputedValue(valueFor20) : setComputedValue(valueFor21);
+    setDisplayResults(true);
+  };
+
+  const handleTaxYearSwitch = (taxYearSwitch) => {
+    taxYearSwitch === 0
+      ? setComputedValue(details.valueFor20)
+      : setComputedValue(details.valueFor21);
+    setTaxYear(taxYearSwitch);
+  };
+
   return (
     <View style={styles.mainContainer}>
       <StatusBar barStyle="dark-content" />
@@ -57,8 +123,8 @@ const App: () => React$Node = () => {
             inputContainerStyle={styles.inputContainer}
             containerStyle={styles.textfieldContainerStyle}
             inputStyle={styles.inputStyle}
-            onChangeText={(value) => {
-              setValue(value);
+            onChangeText={(inputValue) => {
+              setValue(parseInt(inputValue));
               displayResults ? setDisplayResults(false) : null;
             }}
           />
@@ -70,8 +136,18 @@ const App: () => React$Node = () => {
           />
           {displayResults && (
             <View style={styles.resultsContainer}>
+              <ButtonGroup
+                onPress={handleTaxYearSwitch}
+                selectedIndex={taxYear}
+                buttons={['19/20 Tax Year', '20/21 Tax Year']}
+                containerStyle={styles.buttonGroupContainer}
+                selectedButtonStyle={styles.buttonGroupSelectedStyle}
+              />
               <Text style={styles.header}>Your {placeholderDecider()}:</Text>
               <Text style={styles.amount}>{computedValue}£</Text>
+              <Text style={styles.monthly}>
+                Monthly: {(computedValue / 12).toFixed(2)}£
+              </Text>
             </View>
           )}
         </View>
@@ -150,6 +226,13 @@ const styles = StyleSheet.create({
   amount: {
     fontSize: 50,
     fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#161d6f',
+    padding: MARGIN,
+  },
+  monthly: {
+    fontSize: 25,
+    fontWeight: '600',
     textAlign: 'center',
     color: '#161d6f',
     padding: MARGIN,
--- constants.js
+++ constants.js
@@ -1,6 +1,18 @@
 export const ANNUAL_TO_THP = 0;
-export const ANNUAL_TO_THP_COPY = 'Annual salary';
+export const ANNUAL_TO_THP_COPY = 'Annual THP salary';
 export const THP_TO_ANNUAL = 1;
 export const THP_TO_ANNUAL_COPY = 'Take home pay desired';
+
+export const ALLOWANCE = 12500;
+export const BASIC_RATE = 0.2;
+export const HIGHER_RATE = 0.4;
+export const ADDITIONAL_RATE = 0.45;
+
+export const NI_1920_MTH_ALLOWANCE = 719;
+export const NI_2021_MTH_ALLOWANCE = 792;
+export const NI_1920_MTH_UEL = 4167;
+export const NI_2021_MTH_UEL = 4167;
+export const NI_BASIC_RATE = 0.12;
+export const NI_UEL_RATE = 0.2;
 
 export const MARGIN = 10;
